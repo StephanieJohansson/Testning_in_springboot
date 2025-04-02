7182c85ce65b53a199c3a8a6bc0e7867b6e61b2a
StephanieJohansson <3tomu24.johast@folkuniversitetet.nu>
2025-04-02 20:20:41 +0200

    script

---
+++ firstUserDatabase.sql
@@ -0,0 +1,21 @@
+-- CREATE DATABASE orders_db;
+
+USE orders_db;
+
+-- CREATE TABLE Orders (
+  --  id BIGINT AUTO_INCREMENT PRIMARY KEY, 
+ --   userId BIGINT NOT NULL,             
+ --   product VARCHAR(100) NOT NULL,      
+ --   productID BIGINT NOT NULL            
+-- );
+
+-- INSERT INTO Orders (userId, product, productID) VALUES (2, 'Apelsin', 3);
+
+-- UPDATE Orders
+-- SET userId = 1
+-- WHERE userId IS NULL;
+
+SELECT * FROM orders;
+
+SHOW COLUMNS FROM Orders;
+
